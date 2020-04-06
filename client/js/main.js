97e1e6586d9477917b64f8713e252ff201ce966f
Jesús Redondo García <redondogarciajesus@gmail.com>
2020-04-06 18:59:37 +0200

    Chat working

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,40 @@
 'use strict';
 
 import '../css/base-css.css'
-import {Card} from "../../sharedEntities/Card"
+//import {Card} from "../../sharedEntities/Card"
+
+
+//Elements:
+let sendButton = document.querySelector('#bSend');
+let textToRead = document.querySelector('#textToRead');
+let textToSend = document.querySelector('#textToSend');
+
+//Connection:
+let server = new WebSocket("ws://localhost:8081/")
+server.onopen = (event) =>{
+    console.log('Established connection');
+};
+
+server.onerror = (event) => {
+    alert('Error observed '+event);
+}
+
+server.onmessage = (event) =>{
+    textToRead.innerHTML += event.data+'\n';
+}
 
 
 
-let cartaTest = new Card(3,"rojo");
-console.log(cartaTest);
+
+//Sending messages:
+sendButton.addEventListener('click',(evt)=>{
+    console.log(textToSend.value);
+    if(textToSend.value==''){
+        return;
+    }
+
+    server.send(textToSend.value);
+    textToSend.value = '';
+    textToSend.focus();
+
+});
--- server/server.js
+++ server/server.js
@@ -2,12 +2,23 @@
 
 const WebSocket = require('ws');
 
-const wss = WebSocket.Server({port:8081})
+console.log('Starting up the server...');
+const wss = new WebSocket.Server({port:8081})
 
-
+//When connection is open...
 wss.on('connection', (ws)=>{
-   
+    console.log('New client...'); //DEBUG
+    //And there is a message for one client...
     ws.on('message', (data) =>{
-        wss.clients.forEach
+        wss.clients.forEach( (client)=>{ //Broadcast that message.
+            if (client.readyState === WebSocket.OPEN){
+                client.send(data);
+            }
+        })
     })
+
+    ws.on('close',(event)=>{
+        console.log('Client Logged-out'); //DEBUG
+    });
 });
+
